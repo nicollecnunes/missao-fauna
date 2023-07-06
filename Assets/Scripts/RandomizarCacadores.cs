191e7358db1f05662d6bd53654e7c84da6ec33ad
Nicolle <nicollecanuton@gmail.com>
2023-07-06 20:22:31 -0300

    spawn de papeis

--- Assets/Scripts/RandomizarCacadores.cs
+++ Assets/Scripts/RandomizarCacadores.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class RandomizarCacadores : MonoBehaviour
 {
     public GameObject pfb_cacador;
-    public int minCacador, maxCacador;
+    public GameObject pfb_papeis;
+    public int minCacadorPapel, maxCacadorPapel;
     public float areaX, areaZ;
 
     void Start()
     {
-        int qtdCacadores = Random.Range(minCacador, maxCacador + 1);
+        int qtdCacadores = Random.Range(minCacadorPapel, maxCacadorPapel + 1);
+        int qtdPapel = qtdCacadores;
         for (int i = 0; i < qtdCacadores; i++)
         {
             float posX = Random.Range(-areaX/2, areaX/2);
             float posZ = Random.Range(-areaZ/2, areaZ/2);
             Vector3 posicaoCacador = transform.position + new Vector3(posX, 0f, posZ);
             //todo rodar cacadores
-
             GameObject novoCacador = Instantiate(pfb_cacador, posicaoCacador, Quaternion.Euler(0f, 0f, 0f));
+
+            posX = Random.Range(-areaX/2, areaX/2);
+            posZ = Random.Range(-areaZ/2, areaZ/2);
+            Vector3 posicaoPapel = transform.position + new Vector3(posX, 0f, posZ);
+            //todo rodar cacadores
+            GameObject novoPapel = Instantiate(pfb_papeis, posicaoPapel, Quaternion.Euler(0f, 0f, 0f));
         }
     }
 }
